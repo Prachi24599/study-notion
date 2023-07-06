21d459b62f89d95fe15cb75a7b96b835b96960c7
Prachi24599 <prachi24599@gmail.com>
2023-07-06 13:17:28 +0530

    logo and navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/Logo.svg";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex">
+      <Link to="/">
+        <img src={logo} alt="logo" width={160} height={32} loading="lazy" />
+      </Link>
+      <nav>
+        <ul className="flex gap-3">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+          <li>
+            <Link to="/">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
