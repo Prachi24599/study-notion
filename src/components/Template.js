16ac37c86b666ff0d5cc5285fcc5d5ca9f26faf9
Prachi24599 <prachi24599@gmail.com>
2023-07-10 20:39:21 +0530

    style added for template images

--- src/components/Template.js
+++ src/components/Template.js
@@ -6,8 +6,8 @@ import { FcGoogle } from "react-icons/fc";
 
 const Template = ({ title, desc1, desc2, image, formType, setIsLoggedIn }) => {
   return (
-    <div className="flex w-11/12 max-w-[1160px] py-12 mx-auto gap-x-12 gap-y-0">
-      <div className="w-11/12 max-w-[450px]">
+    <div className="flex justify-between w-11/12 max-w-[1160px] py-12 mx-auto gap-x-12 gap-y-0">
+      <div className="w-11/12 max-w-[450px] mx-auto">
         <h1 className="text-richblack-5 font-semibold text-[1.875rem] leading-[2.375rem]">
           {title}
         </h1>
@@ -37,7 +37,7 @@ const Template = ({ title, desc1, desc2, image, formType, setIsLoggedIn }) => {
         </button>
       </div>
 
-      <div>
+      <div className="relative w-11/12 max-w-[450px] mx-auto">
         <img
           src={frameImage}
           alt="Pattern"
@@ -51,6 +51,7 @@ const Template = ({ title, desc1, desc2, image, formType, setIsLoggedIn }) => {
           width={558}
           height={504}
           loading="lazy"
+          className="absolute -top-4 right-4"
         />
       </div>
     </div>
