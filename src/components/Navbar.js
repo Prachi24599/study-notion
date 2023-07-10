fcf5c39d4a9dccb782a162d5e520acb90bdae2d3
Prachi24599 <prachi24599@gmail.com>
2023-07-10 20:04:19 +0530

    Style added for Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,12 +8,12 @@ const Navbar = (props) => {
   let setIsLoggedIn = props.setIsLoggedIn;
 
   return (
-    <div className="flex justify-evenly">
+    <div className="flex justify-between items-center w-11/12 max-w-[1160px] py-4 mx-auto">
       <Link to="/">
         <img src={logo} alt="logo" width={160} height={32} loading="lazy" />
       </Link>
       <nav>
-        <ul className="flex gap-3 ">
+        <ul className="flex gap-x-6 text-richblack-100">
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -26,22 +26,27 @@ const Navbar = (props) => {
         </ul>
       </nav>
       {/* Login Signup Logout Dashboard */}
-      <div className="flex ml-5 mr-3 gap-3">
+      <div className="flex items-center gap-x-4">
         {/* Login button will be visible only when user is not logged in */}
 
         {!isLoggedIn && (
           <Link to="/login">
-            <button>Login</button>
+            <button className="bg-richblack-800 text-richblack-100 py-[8px] px-[12px] rounded-[8px] border border-richblack-700">
+              Log in
+            </button>
           </Link>
         )}
         {!isLoggedIn && (
           <Link to="/signup">
-            <button>Signup</button>
+            <button className="bg-richblack-800 text-richblack-100 py-[8px] px-[12px] rounded-[8px] border border-richblack-700">
+              Sign up
+            </button>
           </Link>
         )}
         {isLoggedIn && (
           <Link to="/">
             <button
+              className="bg-richblack-800 text-richblack-100 py-[8px] px-[12px] rounded-[8px] border border-richblack-700"
               onClick={() => {
                 setIsLoggedIn(false);
                 toast.success("Logged Out");
@@ -53,7 +58,9 @@ const Navbar = (props) => {
         )}
         {isLoggedIn && (
           <Link to="/dashboard">
-            <button>Dashboard</button>
+            <button className="bg-richblack-800 text-richblack-100 py-[8px] px-[12px] rounded-[8px] border border-richblack-700">
+              Dashboard
+            </button>
           </Link>
         )}
       </div>
