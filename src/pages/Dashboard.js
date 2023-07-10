ebe236acc73e85ab5bcbb1136048eee9aa1ef262
Prachi24599 <prachi24599@gmail.com>
2023-07-10 23:57:25 +0530

    height fixed

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className="flex justify-center items-center text-white text-3xl h-full">
+    <div className="flex flex-1 justify-center items-center text-white text-3xl h-full">
       Welcome to StudyNotion
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center text-white text-3xl h-full">
+    <div className="flex flex-1 justify-center items-center text-white text-3xl h-full">
       Home
     </div>
   );
