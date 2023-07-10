e144ba7d1e963665faf65e603c9550c7cc520d5c
Prachi24599 <prachi24599@gmail.com>
2023-07-10 20:28:24 +0530

    Added style to button and Hr

--- src/components/Template.js
+++ src/components/Template.js
@@ -2,6 +2,7 @@ import React from "react";
 import frameImage from "../assets/frame.png";
 import SignupForm from "./SignupForm";
 import LoginForm from "./LoginForm";
+import { FcGoogle } from "react-icons/fc";
 
 const Template = ({ title, desc1, desc2, image, formType, setIsLoggedIn }) => {
   return (
@@ -22,13 +23,16 @@ const Template = ({ title, desc1, desc2, image, formType, setIsLoggedIn }) => {
           <LoginForm setIsLoggedIn={setIsLoggedIn} />
         )}
 
-        <div>
-          <div></div>
-          <p>OR</p>
-          <div></div>
+        <div className="flex w-full items-center my-4 gap-x-2">
+          <div className="w-full h-[1px] bg-richblack-700"></div>
+          <p className="text-richblack-700 font-medium leading-[1.375rem]">
+            OR
+          </p>
+          <div className="w-full h-[1px] bg-richblack-700"></div>
         </div>
 
-        <button>
+        <button className="w-full flex justify-center items-center rounded-[8px] font-medium text-richblack-100 border border-richblack-700 px-[12px] py-[8px] gap-x-2 mt-6">
+          <FcGoogle />
           <p>Sign Up with Google</p>
         </button>
       </div>
